485cd41847e8d5ce8272efa5441245e65037d46c
karankumar <kumarkaran@iitgn.ac.in>
2023-01-23 01:48:57 +0530

    Qn 1

--- metrics.py
+++ metrics.py
@@ -1,46 +1,94 @@
-from typing import Union
-import pandas as pd
-
-
-def accuracy(y_hat: pd.Series, y: pd.Series) -> float:
+import numpy as np
+def accuracy(y_hat, y):
     """
     Function to calculate the accuracy
+    Inputs:
+    > y_hat: pd.Series of predictions
+    > y: pd.Series of ground truth
+    Output:
+    > Returns the accuracy as float
     """
-
     """
     The following assert checks if sizes of y_hat and y are equal.
     Students are required to add appropriate assert checks at places to
     ensure that the function does not fail in corner cases.
     """
-    assert y_hat.size == y.size
+    assert(y_hat.size == y.size)
     # TODO: Write here
-    pass
+    pridct=list(y_hat)
+    Gtruth=list(y)
+    correct_pridiction=0
+    total=len(pridct)
+    for i in range(len(pridct)):
+        if(pridct[i]==Gtruth[i]):
+            correct_pridiction+=1
+    return correct_pridiction/total        
 
-
-def precision(y_hat: pd.Series, y: pd.Series, cls: Union[int, str]) -> float:
+def precision(y_hat, y, cls):
     """
     Function to calculate the precision
+    Inputs:
+    > y_hat: pd.Series of predictions
+    > y: pd.Series of ground truth
+    > cls: The class chosen
+    Output:
+    > Returns the precision as float
     """
-    pass
+    predict=list(y_hat)
+    Gtruth=list(y)
+    Trueclass=0
+    allclass=0
+    for i in range(len(predict)):
+        if(predict[i]==cls):
+            if(predict[i]==Gtruth[i]):
+                Trueclass+=1
+            allclass+=1
+    return Trueclass/allclass            
 
-
-def recall(y_hat: pd.Series, y: pd.Series, cls: Union[int, str]) -> float:
+def recall(y_hat, y, cls):
     """
     Function to calculate the recall
+    Inputs:
+    > y_hat: pd.Series of predictions
+    > y: pd.Series of ground truth
+    > cls: The class chosen
+    Output:
+    > Returns the recall as float
     """
-    pass
+    predict=list(y_hat)
+    Gtruth=list(y)
+    Trueclass=0
+    allclass=0
+    for i in range(len(predict)):
+        if(Gtruth[i]==cls):
+            if(predict[i]==Gtruth[i]):
+                Trueclass+=1
+            allclass+=1
+    return Trueclass/allclass
 
 
-def rmse(y_hat: pd.Series, y: pd.Series) -> float:
+def rmse(y_hat, y):
     """
     Function to calculate the root-mean-squared-error(rmse)
+    Inputs:
+    > y_hat: pd.Series of predictions
+    > y: pd.Series of ground truth
+    Output:
+    > Returns the rmse as float
     """
+    predict=np.array(y_hat)
+    Gtruth=np.array(y)
+    return (np.sqrt(np.mean((predict-Gtruth)**2)))
 
-    pass
-
-
-def mae(y_hat: pd.Series, y: pd.Series) -> float:
+def mae(y_hat, y):
     """
     Function to calculate the mean-absolute-error(mae)
+    Inputs:
+    > y_hat: pd.Series of predictions
+    > y: pd.Series of ground truth
+    Output:
+    > Returns the mae as float
     """
-    pass
+    predict=np.array(y_hat)
+    Gtruth=np.array(y)
+    return (np.mean(abs(predict-Gtruth)))
--- tree/utils.py
+++ tree/utils.py
@@ -1,23 +1,91 @@
-import pandas as pd
-
-
-def entropy(Y: pd.Series) -> float:
+import math
+def entropy(Y):
     """
-    Function to calculate the entropy
+    Function to calculate the entropy 
+    Inputs:
+    > Y: pd.Series of Labels
+    Outpus:
+    > Returns the entropy as a float
     """
+    values=list(Y)
+    total_size=len(Y)
+    group=dict()
+    for i in values:
+        if i in group:
+            group[i]+=1
+        else:
+            group[i]=1
+    etr=0
+    for i in group:
+        prb=group[i]/total_size
+        etr-=prb*math.log2(prb)
+    return etr
 
-    pass
-
-
-def gini_index(Y: pd.Series) -> float:
+def gini_index(Y):
     """
     Function to calculate the gini index
+    Inputs:
+    > Y: pd.Series of Labels
+    Outpus:
+    > Returns the gini index as a float
     """
-    pass
+    values=list(Y)
+    total_size=len(Y)
+    group=dict()
+    for i in values:
+        if i in group:
+            group[i]+=1
+        else:
+            group[i]=1
+    gini=1
+    for i in group:
+        prb=group[i]/total_size
+        gini-=prb**2
+    return gini
 
 
-def information_gain(Y: pd.Series, attr: pd.Series) -> float:
+def information_gain(Y, attr):
     """
     Function to calculate the information gain
+    Inputs:
+    > Y: pd.Series of Labels
+    > attr: pd.Series of attribute at which the gain should be calculated
+    Outputs:
+    > Return the information gain as a float
     """
-    pass
+    group=dict()
+    Y=list(Y)
+    total_size=len(Y)
+    attr=list(attr)
+    for i in range(len(attr)):
+        if attr[i] in group:
+            group[attr[i]].append(Y[i])
+        else:
+            group[attr[i]]=[Y[i]]
+    IG=entropy(Y)
+    for i in group:
+        IG-=(len(group[i])/total_size)*entropy(group[i])
+    return IG  
+
+def gini_diff(Y, attr):
+    """
+    Function to calculate the information gain
+    Inputs:
+    > Y: pd.Series of Labels
+    > attr: pd.Series of attribute at which the gain should be calculated
+    Outputs:
+    > Return the information gain as a float
+    """
+    group=dict()
+    Y=list(Y)
+    total_size=len(Y)
+    attr=list(attr)
+    for i in range(len(attr)):
+        if attr[i] in group:
+            group[attr[i]].append(Y[i])
+        else:
+            group[attr[i]]=[Y[i]]
+    IG=gini_index(Y)
+    for i in group:
+        IG-=(len(group[i])/total_size)*gini_index(group[i])
+    return IG  
